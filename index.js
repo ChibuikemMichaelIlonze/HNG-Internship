cfcf401e8a39ac7717b78ed9eb2faf3f98fd8023
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-09-07 13:56:10 +0100

    set time interval

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const currentDate = new Date();
 const currentDayOfTheWeek = daysOfWeek[currentDate.getDay()];
 
-const currentUtcTime = currentDate.toLocaleString("en-US", {
-  timeZone: "UTC",
-});
+function updateUtcTime() {
+  const currentDate = new Date();
+  const currentUtcTime = currentDate.toLocaleString("en-US", {
+    timeZone: "UTC",
+  });
 
-document.querySelector('.current-day').textContent = currentDayOfTheWeek;
-document.querySelector('.current-utc-time').textContent = currentUtcTime;
+  document.querySelector(".current-utc-time").textContent = currentUtcTime;
+}
+
+updateUtcTime();
+
+setInterval(updateUtcTime, 1000);
+
+document.querySelector(".current-day").textContent = currentDayOfTheWeek;
